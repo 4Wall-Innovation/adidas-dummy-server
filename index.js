34d9c122cccf13946a806614de1d7c4a5df2f5d9
Jake Medler <65961686+JakeLewisMedler@users.noreply.github.com>
2023-07-27 11:15:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+let files = ["data1.xml", "data2.xml"];
+let index = 0;
+
+const nextFile = () => {
+  let file = files[index];
+  index++;
+  if (index >= files.length) index = 0;
+  return file;
+};
+
+app.get("/", (req, res) => {
+  let next = nextFile();
+  console.log("Incoming request! Serving:", next);
+  res.download(next);
+});
+
+app.listen(port, () => {
+  console.log(`Dummy server listening on port ${port}`);
+});
